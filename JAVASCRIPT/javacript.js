acff00aae9abfcbb602dbc20ada6a0bebd9db7f6
unknown <hugo.html.com@gmail.com>
2019-11-29 23:11:52 -0300

    Adicionando animação scroll suável

--- JAVASCRIPT/javacript.js
+++ JAVASCRIPT/javacript.js
@@ -10,6 +10,32 @@ window.onload=function(){
 };
 
 
+/* ANIAMÇAO SCROLL SUAVEL 
+A lógica é simples:
+
+    1 – Identificar o momento em que o usuário clica no link
+    2 – Verificar o valor de href do link e encontrar o elemento com este ID na página
+    3 – Calcular a distância entre o elemento e o topo da página
+    4 – Animar o scroll até o local do elemento.
+
+*/
+
+
+$('#Menu a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
+});
+
+
+
+
+
+
+
 
 
 
